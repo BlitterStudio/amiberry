b8d9f462636a97bfc81a1f43e7ed91463501506e
Dimitris Panokostas <midwan@gmail.com>
2016-12-18 18:07:12 +0100

    Merged sound.h from Chips

--- src/sd-pandora/sound.h
+++ src/sd-pandora/sound.h
@@ -12,8 +12,9 @@
 #define DEFAULT_SOUND_CHANNELS 1
 #endif
 
-#define SOUND_BUFFERS_COUNT 4
-#define SNDBUFFER_LEN 2048
+// These numbers mean SOUND PRODUCER BLOCK count and length
+#define SOUND_BUFFERS_COUNT 32
+#define SNDBUFFER_LEN 256
 
 extern uae_u16 sndbuffer[SOUND_BUFFERS_COUNT][(SNDBUFFER_LEN+32)*DEFAULT_SOUND_CHANNELS];
 extern uae_u16 *sndbufpt;
