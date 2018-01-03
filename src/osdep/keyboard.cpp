3f12881e0c60a894a53256ccf883e803cd94ec47
Dimitris Panokostas <midwan@gmail.com>
2018-01-03 22:11:30 +0100

    Fixed SDL_SCANCODE_NONUSBACKSLASH mapping

--- src/osdep/keyboard.cpp
+++ src/osdep/keyboard.cpp
@@ -511,7 +511,7 @@ static struct uae_input_device_kbr_default keytrans_amiga[] = {
 	{SDL_SCANCODE_END, INPUTEVENT_SPC_QUALIFIER_SPECIAL },
 	{SDL_SCANCODE_PAUSE, INPUTEVENT_SPC_PAUSE },
 
-	{ SDL_SCANCODE_NONUSBACKSLASH, INPUTEVENT_KEY_2B },
+	{ SDL_SCANCODE_NONUSBACKSLASH, INPUTEVENT_KEY_30 },
 
 	// This is configurable, so let's not hardcode it here
 	//{SDL_SCANCODE_F12, INPUTEVENT_SPC_ENTERGUI },
