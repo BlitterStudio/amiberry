aa2882e2b8f6eaa4612af21dba677d9eaabd4341
Dimitris Panokostas <midwan@gmail.com>
2018-02-04 21:04:19 +0100

    Removed beta status from v2.11

--- src/osdep/gui/PanelAbout.cpp
+++ src/osdep/gui/PanelAbout.cpp
@@ -29,7 +29,7 @@ void InitPanelAbout(const struct _ConfigCategory& category)
 {
 	amiberryLogoImage = gcn::Image::load("data/amiberry-logo.png");
 	icon = new gcn::Icon(amiberryLogoImage);
-	lblEmulatorVersion = new gcn::Label("Amiberry v2.11 beta");
+	lblEmulatorVersion = new gcn::Label("Amiberry v2.11");
 
 	textBox = new gcn::TextBox(
 		"Dimitris Panokostas (MiDWaN) - Amiberry author\n"
