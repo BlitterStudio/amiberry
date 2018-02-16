ffffc374bff9032e8f2977727f5dd229515653a9
Dimitris Panokostas <midwan@gmail.com>
2018-02-16 17:27:47 +0100

    Brought back check_prefs_changed_comp()

--- src/jit/compemu_support.cpp
+++ src/jit/compemu_support.cpp
@@ -600,6 +600,30 @@ STATIC_INLINE void alloc_blockinfos(void)
   }
 }
 
+bool check_prefs_changed_comp(bool checkonly)
+{
+	bool changed = 0;
+
+	if (currprefs.compfpu != changed_prefs.compfpu ||
+		currprefs.fpu_strict != changed_prefs.fpu_strict ||
+		currprefs.cachesize != changed_prefs.cachesize)
+		changed = 1;
+
+	if (checkonly)
+		return changed;
+
+	currprefs.compfpu = changed_prefs.compfpu;
+	currprefs.fpu_strict = changed_prefs.fpu_strict;
+
+	if (currprefs.cachesize != changed_prefs.cachesize) {
+		currprefs.cachesize = changed_prefs.cachesize;
+		alloc_cache();
+		changed = 1;
+	}
+
+	return changed;
+}
+
 /********************************************************************
  * Functions to emit data into memory, and other general support    *
  ********************************************************************/
