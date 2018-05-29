5c8dd5533b716d7ab59752d176e24ef445c0ca27
Dimitris Panokostas <midwan@gmail.com>
2018-05-29 21:55:45 +0200

    Updated Blitter functions from latest WinUAE, Fixed #326. Bumped version to v2.22 beta

--- src/osdep/gui/PanelAbout.cpp
+++ src/osdep/gui/PanelAbout.cpp
@@ -29,7 +29,7 @@ void InitPanelAbout(const struct _ConfigCategory& category)
 {
 	amiberryLogoImage = gcn::Image::load("data/amiberry-logo.png");
 	icon = new gcn::Icon(amiberryLogoImage);
-	lblEmulatorVersion = new gcn::Label("Amiberry v2.21 beta");
+	lblEmulatorVersion = new gcn::Label("Amiberry v2.22 beta");
 
 	textBox = new gcn::TextBox(
 		"Dimitris Panokostas (MiDWaN) - Amiberry author\n"
