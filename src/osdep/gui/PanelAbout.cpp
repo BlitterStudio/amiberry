0c44e5f818892f2f6af4d7e9ced2780eaad1c702
Dimitris Panokostas <midwan@gmail.com>
2018-10-06 16:31:37 +0200

    Bumped version to 2.23b

--- src/osdep/gui/PanelAbout.cpp
+++ src/osdep/gui/PanelAbout.cpp
@@ -29,7 +29,7 @@ void InitPanelAbout(const struct _ConfigCategory& category)
 {
 	amiberryLogoImage = gcn::Image::load("data/amiberry-logo.png");
 	icon = new gcn::Icon(amiberryLogoImage);
-	lblEmulatorVersion = new gcn::Label("Amiberry v2.22 beta (2018-06-08)");
+	lblEmulatorVersion = new gcn::Label("Amiberry v2.23 beta (2018-10-06)");
 
 	textBox = new gcn::TextBox(
 		"Dimitris Panokostas (MiDWaN) - Amiberry author\n"
