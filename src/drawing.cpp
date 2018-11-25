953975208af7c1f51668ff54eca9fdbd291ea89e
Dimitris Panokostas <midwan@gmail.com>
2018-11-25 20:09:21 +0100

    Fixed Leds would stop flickering if P96 screen was opened

--- src/drawing.cpp
+++ src/drawing.cpp
@@ -3284,10 +3284,9 @@ void vsync_handle_redraw(void)
 
 		if (framecnt == 0) {
 			init_drawing_frame();
-
-			gui_flicker_led(-1, 0, 0);
 		}
 	}
+	gui_flicker_led(-1, 0, 0);
 }
 
 void hsync_record_line_state(int lineno, enum nln_how how, int changed)
