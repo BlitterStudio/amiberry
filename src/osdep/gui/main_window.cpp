a635fe62a01198a8b6af926fc9cf167670ce0f83
Dimitris Panokostas <midwan@gmail.com>
2019-09-15 16:30:46 +0200

    Brought back software cursor, since several Mali based platforms show no cursor at all in GUI otherwise

--- src/osdep/gui/main_window.cpp
+++ src/osdep/gui/main_window.cpp
@@ -107,6 +107,11 @@ SDL_Event gui_event;
 SDL_Texture* gui_texture;
 SDL_Cursor* cursor;
 SDL_Surface* cursor_surface;
+#ifdef SOFTWARE_CURSOR
+SDL_Texture* swcursor_texture = NULL;
+static SDL_DisplayMode physmode;
+static double mscalex, mscaley;
+#endif // SOFTWARE_CURSOR
 #endif
 
 /*
@@ -225,6 +230,35 @@ static void ShowHelpRequested()
 	}
 }
 
+#ifdef SOFTWARE_CURSOR
+static SDL_Rect dst;
+void swcursor(bool op) {
+	if (op == -1) {
+		SDL_DestroyTexture(swcursor_texture);
+		swcursor_texture = NULL;
+
+	}
+	else if (op == 0) {
+		cursor_surface = SDL_LoadBMP("data/cursor.bmp");
+		swcursor_texture = SDL_CreateTextureFromSurface(renderer, cursor_surface);
+		// Hide real cursor
+		SDL_SetCursor(SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_ARROW));
+		SDL_ShowCursor(0);
+		// Set cursor width,height to that of loaded bmp
+		dst.w = cursor_surface->w;
+		dst.h = cursor_surface->h;
+		SDL_FreeSurface(cursor_surface);
+
+	}
+	else {
+		SDL_GetMouseState(&dst.x, &dst.y);
+		dst.x *= mscalex * 1.03;
+		dst.y *= mscaley * 1.005;
+		SDL_RenderCopy(renderer, swcursor_texture, nullptr, &dst);
+	}
+}
+#endif
+
 void UpdateGuiScreen()
 {
 #ifdef USE_SDL1
@@ -233,6 +267,9 @@ void UpdateGuiScreen()
 #elif USE_SDL2
 	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer, gui_texture, nullptr, nullptr);
+#ifdef SOFTWARE_CURSOR
+	swcursor(1);
+#endif
 	SDL_RenderPresent(renderer);
 #endif
 }
@@ -298,7 +335,11 @@ namespace sdl
 		SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
 		SDL_ShowCursor(SDL_ENABLE);
 #elif USE_SDL2
-#ifdef NO_CUSTOM_CURSOR
+#ifdef SOFTWARE_CURSOR
+		swcursor(0);
+		SDL_GetCurrentDisplayMode(0, &physmode);
+		mscalex = ((double)GUI_WIDTH / (double)physmode.w);
+		mscaley = ((double)GUI_HEIGHT / (double)physmode.h);
 #else
 		setup_cursor();
 #endif
@@ -378,6 +419,13 @@ namespace sdl
 			cursor = nullptr;
 		}
 
+#ifdef SOFTWARE_CURSOR
+		if (cursor_surface)
+		{
+			SDL_FreeSurface(cursor_surface);
+			cursor_surface = nullptr;
+		}
+#endif
 		// Clear the screen
 		SDL_RenderClear(renderer);
 		SDL_RenderPresent(renderer);
