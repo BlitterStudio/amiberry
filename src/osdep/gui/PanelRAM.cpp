9deeed16a2fe147dc77b9e5b067b259469f83cad
Dimitris Panokostas <midwan@gmail.com>
2019-10-01 19:23:37 +0200

    Increased maximum Processor slot RAM to 128MB

--- src/osdep/gui/PanelRAM.cpp
+++ src/osdep/gui/PanelRAM.cpp
@@ -17,18 +17,18 @@
 #include "gui_handling.h"
 
 
-static const char* ChipMem_list[] = {"512 K", "1 MB", "2 MB", "4 MB", "8 MB"};
-static unsigned int ChipMem_values[] = {0x080000, 0x100000, 0x200000, 0x400000, 0x800000};
-static const char* SlowMem_list[] = {"None", "512 K", "1 MB", "1.5 MB", "1.8 MB"};
-static unsigned int SlowMem_values[] = {0x000000, 0x080000, 0x100000, 0x180000, 0x1c0000};
-static const char* FastMem_list[] = {"None", "1 MB", "2 MB", "4 MB", "8 MB", "16 MB", "32 MB", "64 MB", "128 MB"};
+static const char* ChipMem_list[] = { "512 K", "1 MB", "2 MB", "4 MB", "8 MB" };
+static unsigned int ChipMem_values[] = { 0x080000, 0x100000, 0x200000, 0x400000, 0x800000 };
+static const char* SlowMem_list[] = { "None", "512 K", "1 MB", "1.5 MB", "1.8 MB" };
+static unsigned int SlowMem_values[] = { 0x000000, 0x080000, 0x100000, 0x180000, 0x1c0000 };
+static const char* FastMem_list[] = { "None", "1 MB", "2 MB", "4 MB", "8 MB", "16 MB", "32 MB", "64 MB", "128 MB" };
 static unsigned int FastMem_values[] = {
 	0x000000, 0x100000, 0x200000, 0x400000, 0x800000, 0x1000000, 0x2000000, 0x4000000, 0x8000000
 };
-static const char* A3000LowMem_list[] = {"None", "8 MB", "16 MB"};
-static unsigned int A3000LowMem_values[] = {0x080000, 0x800000, 0x1000000};
-static const char* A3000HighMem_list[] = {"None", "8 MB", "16 MB", "32 MB"};
-static unsigned int A3000HighMem_values[] = {0x080000, 0x800000, 0x1000000, 0x2000000};
+static const char* A3000LowMem_list[] = { "None", "8 MB", "16 MB" };
+static unsigned int A3000LowMem_values[] = { 0x080000, 0x800000, 0x1000000 };
+static const char* A3000HighMem_list[] = { "None", "8 MB", "16 MB", "32 MB", "64 MB", "128 MB" };
+static unsigned int A3000HighMem_values[] = { 0x080000, 0x800000, 0x1000000, 0x2000000, 0x4000000, 0x8000000 };
 
 static gcn::Window* grpRAM;
 static gcn::Label* lblChipmem;
@@ -186,7 +186,7 @@ void InitPanelRAM(const struct _ConfigCategory& category)
 	lblA3000Lowsize = new gcn::Label("None   ");
 
 	lblA3000Highmem = new gcn::Label("A4000 Proc. board:");
-	sldA3000Highmem = new gcn::Slider(0, 3);
+	sldA3000Highmem = new gcn::Slider(0, 5);
 	sldA3000Highmem->setSize(sldWidth, SLIDER_HEIGHT);
 	sldA3000Highmem->setBaseColor(gui_baseCol);
 	sldA3000Highmem->setMarkerLength(markerLength);
@@ -342,7 +342,7 @@ void RefreshPanelRAM()
 		}
 	}
 
-	for (i = 0; i < 4; ++i)
+	for (i = 0; i < 6; ++i)
 	{
 		if (changed_prefs.mbresmem_high_size == A3000HighMem_values[i])
 		{
