a1a9fb40b777d90db7023ce3eade6963589f5c51
Dimitris Panokostas <midwan@gmail.com>
2019-10-18 20:05:48 +0200

    Fix compiler warning "memory order argument to atomic operation is invalid" in AARCH64 builds

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -130,29 +130,29 @@ STATIC_INLINE int max(int x, int y)
 
 STATIC_INLINE void atomic_and(volatile uae_atomic *p, uae_u32 v)
 {
-	__atomic_and_fetch(p, v, __ATOMIC_ACQ_REL);
+	__atomic_and_fetch(p, v, __ATOMIC_SEQ_CST);
 }
 STATIC_INLINE void atomic_or(volatile uae_atomic *p, uae_u32 v)
 {
-	__atomic_or_fetch(p, v, __ATOMIC_ACQ_REL);
+	__atomic_or_fetch(p, v, __ATOMIC_SEQ_CST);
 }
 STATIC_INLINE uae_atomic atomic_inc(volatile uae_atomic *p)
 {
-	return __atomic_add_fetch(p, 1, __ATOMIC_ACQ_REL);
+	return __atomic_add_fetch(p, 1, __ATOMIC_SEQ_CST);
 }
 STATIC_INLINE uae_atomic atomic_dec(volatile uae_atomic *p)
 {
-	return __atomic_sub_fetch(p, 1, __ATOMIC_ACQ_REL);
+	return __atomic_sub_fetch(p, 1, __ATOMIC_SEQ_CST);
 }
 STATIC_INLINE uae_u32 atomic_bit_test_and_reset(volatile uae_atomic *p, uae_u32 v)
 {
   uae_u32 mask = (1 << v);
-  uae_u32 res = __atomic_fetch_and(p, ~mask, __ATOMIC_ACQ_REL);
+  uae_u32 res = __atomic_fetch_and(p, ~mask, __ATOMIC_SEQ_CST);
 	return (res & mask);
 }
 STATIC_INLINE void atomic_set(volatile uae_atomic *p, uae_u32 v)
 {
-  __atomic_store_n(p, v, __ATOMIC_ACQ_REL);
+  __atomic_store_n(p, v, __ATOMIC_SEQ_CST);
 }
 
 #else
