dbbc33f7822c70f5de575d0bbec8e4208d0c6a91
Dimitris Panokostas <midwan@gmail.com>
2020-02-01 20:50:48 +0100

    Bump version to 3.1.0

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -22,8 +22,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry v3.0.11 (2019-12-15)")
-#define AMIBERRYDATE MAKEBD(2019, 12, 01)
+#define AMIBERRYVERSION _T("Amiberry v3.1.0 (2020-02-01)")
+#define AMIBERRYDATE MAKEBD(2020, 02, 01)
 
 extern std::string get_version_string();
 
