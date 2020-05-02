47fcd8e6c1a3c0726c496cfff9b165df663b505d
Dimitris Panokostas <midwan@gmail.com>
2020-05-03 00:07:26 +0200

    Set Amiberry version to next beta

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -22,7 +22,7 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry v3.1.3.1 (2020-04-30)")
+#define AMIBERRYVERSION _T("Amiberry v3.1.4 beta (2020-05-02)")
 #define AMIBERRYDATE MAKEBD(2020, 03, 26)
 
 extern std::string get_version_string();
