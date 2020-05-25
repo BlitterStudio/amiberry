2258c6e9e28dacfb83f269c4ba6061bf75cc1fbf
Dimitris Panokostas <midwan@gmail.com>
2020-05-26 00:56:46 +0200

    Updated version date

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -22,8 +22,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry v3.2 beta (2020-05-25)")
-#define AMIBERRYDATE MAKEBD(2020, 5, 25)
+#define AMIBERRYVERSION _T("Amiberry v3.2 beta (2020-05-26)")
+#define AMIBERRYDATE MAKEBD(2020, 5, 26)
 
 extern std::string get_version_string();
 
