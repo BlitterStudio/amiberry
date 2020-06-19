3aa787d696058b6bacf7c4bbc87d7d867728c77e
Dimitris Panokostas <midwan@gmail.com>
2020-06-20 00:00:50 +0200

    More header updates

--- src/include/uae/mman.h
+++ src/include/uae/mman.h
@@ -3,6 +3,29 @@
 
 #include "uae/types.h"
 
+#define MAX_SHMID 256
+
+typedef int uae_key_t;
+
+/* One shmid data structure for each shared memory segment in the system. */
+struct uae_shmid_ds {
+	uae_key_t key;
+	uae_u32 size;
+	uae_u32 rosize;
+	void* addr;
+	TCHAR name[MAX_DPATH];
+	void* attached;
+	int mode;
+	void* natmembase;
+	bool fake;
+	int maprom;
+};
+
+void* uae_shmat(addrbank* ab, int shmid, void* shmaddr, int shmflg, struct uae_mman_data* md);
+int uae_shmdt(const void* shmaddr);
+int uae_shmget(uae_key_t key, addrbank* ab, int shmflg);
+int uae_shmctl(int shmid, int cmd, struct uae_shmid_ds* buf);
+
 #define UAE_IPC_PRIVATE 0x01
 #define UAE_IPC_RMID    0x02
 #define UAE_IPC_CREAT   0x04
@@ -14,6 +37,7 @@ struct uae_mman_data
 	uae_u32 size;
 	bool readonly;
 	uae_u32 readonlysize;
+	bool maprom;
 	bool directsupport;
 	bool hasbarrier;
 };
