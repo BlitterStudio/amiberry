655b52d683d4bf08c5efea9a2b815fc4882efa83
Dimitris Panokostas <midwan@gmail.com>
2020-07-06 02:06:05 +0200

    Comment out not-yet-ready case in emulib

--- src/uaelib.cpp
+++ src/uaelib.cpp
@@ -428,7 +428,7 @@ static uae_u32 uaelib_demux_common(TrapContext* ctx, uae_u32 ARG0, uae_u32 ARG1,
 		if (currprefs.allow_host_run)
 			return emulib_run_on_host(ctx, ARG1);
 		return 0;
-	case 89: return emulib_host_session(ctx, ARG1, ARG2, ARG3);
+	//case 89: return emulib_host_session(ctx, ARG1, ARG2, ARG3);
 	}
 	
 	return 0;
