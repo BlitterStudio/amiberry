24676f29ad64e2e0e7116aaaaaf9003f27ca9321
Dimitris Panokostas <midwan@gmail.com>
2021-04-02 11:17:17 +0200

    Changed version strings to next beta

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -24,8 +24,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry v4.0 (2021-04-01)")
-#define AMIBERRYDATE MAKEBD(2021, 4, 1)
+#define AMIBERRYVERSION _T("Amiberry v4.x beta (2021-04-02)")
+#define AMIBERRYDATE MAKEBD(2021, 4, 2)
 
 #define IHF_WINDOWHIDDEN 6
 
