d8c47a3456983e676ee4a9f570d26d959ee3c718
Dimitris Panokostas <midwan@gmail.com>
2021-08-21 21:17:46 +0200

    Retroarch: Fixed crash on startup when parsing controller file
    This should only occur when using more recent SDL2 versions which support more controller buttons!

--- src/osdep/amiberry_input.h
+++ src/osdep/amiberry_input.h
@@ -85,5 +85,4 @@ extern bool init_kb_from_retroarch(int index, char* retroarch_file);
 extern std::string sanitize_retroarch_name(std::string s);
 extern int find_retroarch(const TCHAR* find_setting, char* retroarch_file);
 extern bool find_retroarch_polarity(const TCHAR* find_setting, char* retroarch_file);
-extern std::string binding_from_retroarch(int cpt, char* control_config);
 extern host_input_button map_from_retroarch(host_input_button mapping, char* control_config, int player);
