9bd72e2e6da87fa428e88e5a9fb0a45e7a0c88fa
Dimitris Panokostas <midwan@gmail.com>
2021-09-14 17:30:27 +0200

    Merged latest changes from @gsfare
    Still WIP

--- src/parser.cpp
+++ src/parser.cpp
@@ -56,6 +56,7 @@
 #ifdef POSIX_SERIAL
 #include <termios.h>
 #include <unistd.h>
+#include <sys/epoll.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/select.h>
@@ -263,16 +264,16 @@ void doprinter (uae_u8 val)
 
 struct uaeserialdata
 {
-	long hCom;
-	long evtr, evtw, evtt, evtwce;
-	long olr, olw, olwce;
+	int fd;
 	int writeactive;
 	void *readdata, *writedata;
+	int readatasize, writedatasize;
 	volatile int threadactive;
-	uae_sem_t change_sem, sync_sem;
-	int fd;
+	uae_sem_t change_sem, sync_sem, evtt;
 	void *user;
 	struct serial_icounter_struct serial_counter;
+
+	uae_sem_t write_start, write_done;
 };
 
 int uaeser_getdatalength (void)
@@ -412,31 +413,43 @@ int uaeser_setparams (void *vsd, int baud, int rbuffer, int bits, int sbits, int
 
 	/* Set new settings */
 	if (tcsetattr(sd->fd, TCSANOW, &newtio) < 0)
-		return 0;
+		return 1;
 
 	/*
 	 * TODO
 	 * What to do with rbuffer?
 	 */
-	return 1;
+
+	/* Return logic is reversed in this function! */
+	return 0;
 }
 
-static void *uaeser_trap_thread (void *arg)
+static int uaeser_trap_thread (void *arg)
 {
-#ifndef AMIBERRY
 	struct uaeserialdata* sd = (struct uaeserialdata*)arg;
-	HANDLE handles[4];
+	//HANDLE handles[4];
 	int cnt;
-	DWORD evtmask, actual;
+	//DWORD evtmask, actual;
 	
 	uae_set_thread_priority(NULL, 1);
 	sd->threadactive = 1;
 	uae_sem_post(&sd->sync_sem);
-	startwce(sd, &evtmask);
+	//startwce(sd, &evtmask);
 	while (sd->threadactive == 1) {
 		int sigmask = 0;
 		uae_sem_wait(&sd->change_sem);
-		if (WaitForSingleObject(sd->evtwce, 0) == WAIT_OBJECT_0) {
+
+		struct timeval tv;
+		fd_set fd;
+		tv.tv_sec = 0;
+		tv.tv_usec = 0;
+		FD_ZERO(&fd);
+		FD_SET(sd->fd, &fd);
+		int num_ready = select (FD_SETSIZE, &fd, NULL, NULL, &tv);
+		if( num_ready > 0 )
+				sigmask |= 1;
+
+		/*if (WaitForSingleObject(sd->evtwce, 0) == WAIT_OBJECT_0) {
 			if (evtmask & EV_RXCHAR)
 				sigmask |= 1;
 			if ((evtmask & EV_TXEMPTY) && !sd->writeactive)
@@ -444,55 +457,78 @@ static void *uaeser_trap_thread (void *arg)
 			if (evtmask & EV_BREAK)
 				sigmask |= 4;
 			startwce(sd, &evtmask);
-		}		
+		}
 		cnt = 0;
 		handles[cnt++] = sd->evtt;
 		handles[cnt++] = sd->evtwce;
 		if (sd->writeactive) {
-			if (GetOverlappedResult(sd->hCom, &sd->olw, &actual, FALSE)) {
+			if (GetOverlappedResult (sd->hCom, &sd->olw, &actual, FALSE)) {
 				sd->writeactive = 0;
 				sigmask |= 2;
-			}
-			else {
+			} else {
 				handles[cnt++] = sd->evtw;
 			}
 		}
+		*/
 		if (!sd->writeactive)
 			sigmask |= 2;
-		uaeser_signal(sd->user, sigmask | 1);
+
+		uaeser_signal (sd->user, sigmask);
 		uae_sem_post(&sd->change_sem);
-		WaitForMultipleObjects(cnt, handles, FALSE, INFINITE);
+		//WaitForMultipleObjects(cnt, handles, FALSE, INFINITE);
+		uae_sem_wait (&sd->evtt);
 	}
 	sd->threadactive = 0;
 	uae_sem_post(&sd->sync_sem);
-#endif
+
+	return( 0 );
 }
 
 void uaeser_trigger (void *vsd)
 {
-	struct uaeserialdatawin32* sd = (struct uaeserialdatawin32*)vsd;
-	
-	/*
-	 * TODO
-	 * What to do with this? There is some sync/aysnc Win32 stuff required here
-	 * but I don't know how this relates to POSIX serial devices yet. Related to
-	 * uaeser_trap_thread().
-	 */
+	struct uaeserialdata *sd = (struct uaeserialdata*)vsd;
+	uae_sem_post (&sd->evtt);
 }
 
 int uaeser_write (void *vsd, uae_u8 *data, uae_u32 len)
 {
 	struct uaeserialdata* sd = (struct uaeserialdata*)vsd;
-	if (write(sd->fd, data, len) != len)
-		return 0;
-	return 1;
+	int ret = 1;
+
+	if (sd->writeactive==0)
+	{
+
+		sd->writedata=data;
+		sd->writedatasize=len;
+		uae_sem_post (&sd->write_start);
+		//uae_sem_wait (&sd->write_done);
+	}
+
+	//uae_sem_post (&sd->evtt);
+
+	return ret;
 }
 
 int uaeser_read (void *vsd, uae_u8 *data, uae_u32 len)
 {
 	struct uaeserialdata* sd = (struct uaeserialdata*)vsd;
+
+	struct timeval tv;
+	fd_set fd;
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+	FD_ZERO(&fd);
+	FD_SET(sd->fd, &fd);
+	int num_ready = select (FD_SETSIZE, &fd, NULL, NULL, &tv);
+
+	if (len>num_ready)
+		return 0;
+
 	if (read(sd->fd, data, len) != len)
 		return 0;
+
+	//uae_sem_post (&sd->evtt);
+
 	return 1;
 }
 
@@ -502,6 +538,74 @@ void uaeser_clearbuffers (void *vsd)
 	tcflush(sd->fd, TCOFLUSH);
 }
 
+static int serial_read_thread(void* data)
+{
+	struct uaeserialdata* sd = (struct uaeserialdata*)data;
+	int efd, n, available_bytes, bytes_read;
+	struct epoll_event event;
+	struct epoll_event* events;
+	char buffer[256];
+
+	event.data.fd = sd->fd;
+	event.events = EPOLLIN | EPOLLET;
+
+	efd = epoll_create1(0);
+	epoll_ctl(efd, EPOLL_CTL_ADD, sd->fd, &event);
+
+	/* Buffer where events are returned */
+	events = (struct epoll_event*)calloc(64, sizeof event);
+
+	while (1)
+	{
+		n = epoll_wait(efd, events, 64, 5000);
+		if (n > 0)
+		{
+			ioctl(sd->fd, FIONREAD, &available_bytes);
+			if (available_bytes > 0)
+			{
+				uae_sem_post(&sd->evtt);
+				printf("Data available: %d bytes\n", available_bytes);
+			}
+		}
+	}
+
+	free(events);
+
+	return(0);
+}
+
+static int serial_write_thread( void *data )
+{
+	struct uaeserialdata *sd = (struct uaeserialdata*)data;
+	int total_written = 0, written = 0;
+
+	while (1)
+	{
+		uae_sem_wait (&sd->write_start);
+
+		sd->writeactive = 1;
+
+		do {
+			written = write (sd->fd, sd->writedata, sd->writedatasize);
+			if( written < 0 )
+				break;
+
+			total_written += written;
+		}
+		while (total_written < sd->writedatasize);
+
+		tcdrain (sd->fd);
+
+		sd->writeactive = 0;
+		written = 0;
+		total_written = 0;
+
+	  uae_sem_post (&sd->evtt);
+	}
+
+	return 0;
+}
+
 int uaeser_open (void *vsd, void *user, int unit)
 {
 	struct uaeserialdata* sd = (struct uaeserialdata*)vsd;
@@ -509,7 +613,8 @@ int uaeser_open (void *vsd, void *user, int unit)
 	struct serial_icounter_struct serial_counter;
 	char ser[128];
 
-	snprintf(ser, 64, "/dev/ttyS%d", unit);
+	snprintf (ser, 64, "/dev/ttyUSB%d", unit);
+
 	sd->fd = open(ser, O_RDWR | O_NONBLOCK | O_BINARY, 0);
 
 	newtio.c_cflag = B115200 | CS8 | CLOCAL | CREAD;
@@ -538,18 +643,49 @@ int uaeser_open (void *vsd, void *user, int unit)
 	if (sd->fd < 0)
 		return 0;
 
+	sd->user = user;
+
+	uae_sem_init (&sd->sync_sem, 0, 0);
+	uae_sem_init (&sd->change_sem, 0, 1);
+	uae_sem_init (&sd->evtt, 0, 0);
+	uae_start_thread (_T("uaeserial_amiberry"), uaeser_trap_thread, sd, NULL);
+	uae_start_thread (_T("uaeserial_readthread"), serial_read_thread, sd, NULL);
+	uae_start_thread (_T("uaeserial_writethread"), serial_write_thread, sd, NULL);
+	uae_sem_wait (&sd->sync_sem);
+
+
+	uae_sem_init (&sd->write_start, 0, 0);
+	uae_sem_init (&sd->write_done, 0, 0);
+
 	return 1;
 }
 
-void uaeser_close (void *vsd)
+void uaeser_close(void* vsd)
 {
 	struct uaeserialdata* sd = (struct uaeserialdata*)vsd;
+
+	if (sd->threadactive) {
+		sd->threadactive = -1;
+		uae_sem_post(&sd->evtt);
+		while (sd->threadactive)
+			sleep(10);
+		uae_sem_destroy(&sd->evtt);
+	}
 
 	if (sd->fd >= 0)
 		close(sd->fd);
 
+	uae_sem_destroy(&sd->sync_sem);
+	uae_sem_destroy(&sd->change_sem);
+
+	uae_sem_destroy(&sd->write_start);
+	uae_sem_destroy(&sd->write_done);
+
+
 	uaeser_initdata(sd, sd->user);
 }
+
+
 
 #ifdef AMIBERRY
 /* No MIDI support. */
