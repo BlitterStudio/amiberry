fca056dbdb532019ccb231f154592d08a83c3315
Dimitris Panokostas <midwan@gmail.com>
2021-12-03 14:51:31 +0100

    Updated beta version date

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -24,8 +24,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry v4.2 beta (2021-11-29)")
-#define AMIBERRYDATE MAKEBD(2021, 11, 29)
+#define AMIBERRYVERSION _T("Amiberry v4.2 beta (2021-12-03)")
+#define AMIBERRYDATE MAKEBD(2021, 12, 3)
 
 #define IHF_WINDOWHIDDEN 6
 
