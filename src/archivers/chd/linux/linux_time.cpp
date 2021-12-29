346478611906b198eac43d092f90a8ccb43f12c2
Dimitris Panokostas <midwan@gmail.com>
2021-12-29 21:37:49 +0100

    Completed first version of Linux-specific linux_time file

--- src/archivers/chd/linux/linux_time.cpp
+++ src/archivers/chd/linux/linux_time.cpp
@@ -33,7 +33,7 @@ static BOOL using_qpc = TRUE;
 
 osd_ticks_t osd_ticks(void)
 {
-	LARGE_INTEGER performance_count;
+	Uint64 performance_count;
 
 	// if we're suspended, just return that
 	if (suspend_ticks != 0)
@@ -42,22 +42,15 @@ osd_ticks_t osd_ticks(void)
 	// if we have a per second count, just go for it
 	if (ticks_per_second != 0)
 	{
-		// QueryPerformanceCounter if we can
-		if (using_qpc)
-		{
-			QueryPerformanceCounter(&performance_count);
-			return (osd_ticks_t)performance_count.QuadPart - suspend_ticks;
-		}
-
-		// otherwise, fall back to timeGetTime
-		else
-			return (osd_ticks_t)timeGetTime() - suspend_ticks;
+		performance_count = SDL_GetPerformanceCounter();
+		return performance_count - suspend_ticks;
 	}
 
 	// if not, we have to determine it
-	using_qpc = QueryPerformanceFrequency(&performance_count) && (performance_count.QuadPart != 0);
+	performance_count = SDL_GetPerformanceFrequency();
+	using_qpc = performance_count != 0;
 	if (using_qpc)
-		ticks_per_second = (osd_ticks_t)performance_count.QuadPart;
+		ticks_per_second = performance_count;
 	else
 		ticks_per_second = 1000;
 
@@ -84,28 +77,23 @@ osd_ticks_t osd_ticks_per_second(void)
 
 void osd_sleep(osd_ticks_t duration)
 {
-	DWORD msec;
-
 	// make sure we've computed ticks_per_second
 	if (ticks_per_second == 0)
 		(void)osd_ticks();
 
 	// convert to milliseconds, rounding down
-	msec = (DWORD)(duration * 1000 / ticks_per_second);
+	auto msec = (duration * 1000 / ticks_per_second);
 
 	// only sleep if at least 2 full milliseconds
 	if (msec >= 2)
 	{
-		HANDLE current_thread = GetCurrentThread();
-		int old_priority = GetThreadPriority(current_thread);
-
 		// take a couple of msecs off the top for good measure
 		msec -= 2;
 
 		// bump our thread priority super high so that we get
 		// priority when we need it
-		SetThreadPriority(current_thread, THREAD_PRIORITY_TIME_CRITICAL);
+		SDL_SetThreadPriority(SDL_THREAD_PRIORITY_HIGH);
 		Sleep(msec);
-		SetThreadPriority(current_thread, old_priority);
+		SDL_SetThreadPriority(SDL_THREAD_PRIORITY_NORMAL);
 	}
 }
