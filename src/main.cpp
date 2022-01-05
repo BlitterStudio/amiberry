bb76abdebfb74f56a100899bc108c8142c7d4942
Dimitris Panokostas <midwan@gmail.com>
2022-01-06 00:31:53 +0100

    Updated version date

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@
 #include <sys/ioctl.h>
 #include "keyboard.h"
 
-static const char __ver[40] = "$VER: Amiberry 4.3 beta (2022-01-01)";
+static const char __ver[40] = "$VER: Amiberry 4.3 beta (2022-01-06)";
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
 struct uae_prefs currprefs, changed_prefs;
