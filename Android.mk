9bec6ba73ba0aeafb0ca0d33ebec9ba9c952c525
Dimitris Panokostas <midwan@gmail.com>
2022-01-07 12:38:25 +0100

    Synced all makefiles with latest changes

--- Android.mk
+++ Android.mk
@@ -29,18 +29,13 @@ else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 endif
 
 #LOCAL_CPPFLAGS := -std=gnu++14 -pipe -frename-registers 
-LOCAL_CPPFLAGS := -std=c++14 -pipe -frename-registers \
+LOCAL_CPPFLAGS := -std=c++17 -pipe -frename-registers \
                     -Wno-shift-overflow -Wno-narrowing
 
 LOCAL_LDFLAGS += -fuse-ld=gold
 
 # Add your application source files here...
-LOCAL_SRC_FILES := src/archivers/7z/BraIA64.c \
-                   	src/archivers/7z/Delta.c \
-                   	src/archivers/7z/Sha256.c \
-                   	src/archivers/7z/XzCrc64.c \
-                   	src/archivers/7z/XzDec.c \
-                   	src/akiko.cpp \
+LOCAL_SRC_FILES := src/akiko.cpp \
                     src/ar.cpp \
                     src/audio.cpp \
                     src/autoconf.cpp \
@@ -83,11 +78,13 @@ LOCAL_SRC_FILES := src/archivers/7z/BraIA64.c \
                     src/ide.cpp \
                     src/ini.cpp \
                     src/inputdevice.cpp \
+                    src/inputrecord.cpp \
 					src/isofs.cpp \
                     src/keybuf.cpp \
                     src/main.cpp \
                     src/memory.cpp \
                     src/native2amiga.cpp \
+                    src/parser.cpp \
                     src/rommgr.cpp \
                     src/rtc.cpp \
                     src/savestate.cpp \
@@ -95,6 +92,7 @@ LOCAL_SRC_FILES := src/archivers/7z/BraIA64.c \
                     src/scsi.cpp \
 					src/scsiemul.cpp \
                     src/scsitape.cpp \
+                    src/serial_win32.cpp \
                     src/statusline.cpp \
 					src/tabletlibrary.cpp \
 					src/tinyxml2.cpp \
@@ -105,19 +103,70 @@ LOCAL_SRC_FILES := src/archivers/7z/BraIA64.c \
                     src/uaeresource.cpp \
                     src/zfile.cpp \
                     src/zfile_archive.cpp \
-                    src/archivers/7z/7zAlloc.cpp \
-                    src/archivers/7z/7zBuf.cpp \
-                    src/archivers/7z/7zCrc.cpp \
-                    src/archivers/7z/7zCrcOpt.cpp \
-                    src/archivers/7z/7zDec.cpp \
-                    src/archivers/7z/7zIn.cpp \
-                    src/archivers/7z/7zStream.cpp \
-                    src/archivers/7z/Bcj2.cpp \
-                    src/archivers/7z/Bra.cpp \
-                    src/archivers/7z/Bra86.cpp \
-                    src/archivers/7z/LzmaDec.cpp \
-                    src/archivers/7z/Lzma2Dec.cpp \
-                    src/archivers/7z/Xz.cpp \
+                    src/archivers/7z/7zAlloc.c \
+                    src/archivers/7z/7zArcIn.c \
+                    src/archivers/7z/7zBuf.c \
+                    src/archivers/7z/7zBuf2.c \
+                    src/archivers/7z/7zCrc.c \
+                    src/archivers/7z/7zCrcOpt.c \
+                    src/archivers/7z/7zDec.c \
+                    src/archivers/7z/7zFile.c \
+                    src/archivers/7z/7zStream.c \
+                    src/archivers/7z/Aes.c \
+                    src/archivers/7z/AesOpt.c \
+                    src/archivers/7z/Alloc.c \
+                    src/archivers/7z/Bcj2.c \
+                    src/archivers/7z/Bra.c \
+                    src/archivers/7z/Bra86.c \
+                    src/archivers/7z/BraIA64.c \
+                    src/archivers/7z/CpuArch.c \
+                    src/archivers/7z/Delta.c \
+                    src/archivers/7z/LzFind.c \
+                    src/archivers/7z/Lzma2Dec.c \
+                    src/archivers/7z/Lzma2Enc.c \
+                    src/archivers/7z/Lzma86Dec.c \
+                    src/archivers/7z/Lzma86Enc.c \
+                    src/archivers/7z/LzmaDec.c \
+                    src/archivers/7z/LzmaEnc.c \
+                    src/archivers/7z/LzmaLib.c \
+                    src/archivers/7z/Ppmd7.c \
+                    src/archivers/7z/Ppmd7Dec.c \
+                    src/archivers/7z/Ppmd7Enc.c \
+                    src/archivers/7z/Sha256.c \
+                    src/archivers/7z/Sort.c \
+                    src/archivers/7z/Xz.c \
+                    src/archivers/7z/XzCrc64.c \
+                    src/archivers/7z/XzCrc64Opt.c \
+                    src/archivers/7z/XzDec.c \
+                    src/archivers/7z/XzEnc.c \
+                    src/archivers/7z/XzIn.c \
+                    src/archivers/chd/avhuff.cpp \
+                    src/archivers/chd/bitmap.cpp \
+                    src/archivers/chd/cdrom.cpp \
+                    src/archivers/chd/chd.cpp \
+                    src/archivers/chd/chdcd.cpp \
+                    src/archivers/chd/chdcodec.cpp \
+                    src/archivers/chd/corealloc.cpp \
+                    src/archivers/chd/corefile.cpp \
+                    src/archivers/chd/corestr.cpp \
+                    src/archivers/chd/flac.cpp \
+                    src/archivers/chd/harddisk.cpp \
+                    src/archivers/chd/hashing.cpp \
+                    src/archivers/chd/huffman.cpp \
+                    src/archivers/chd/md5.cpp \
+                    src/archivers/chd/osdcore.cpp \
+                    src/archivers/chd/osdlib_unix.cpp \
+                    src/archivers/chd/osdsync.cpp \
+                    src/archivers/chd/palette.cpp \
+                    src/archivers/chd/posixdir.cpp \
+                    src/archivers/chd/posixfile.cpp \
+                    src/archivers/chd/posixptty.cpp \
+                    src/archivers/chd/posixsocket.cpp \
+                    src/archivers/chd/strconv.cpp \
+                    src/archivers/chd/strformat.cpp \
+                    src/archivers/chd/unicode.cpp \
+                    src/archivers/chd/vecstream.cpp \
+                    src/archivers/chd/utf8proc.c \
                     src/archivers/dms/crc_csum.cpp \
                     src/archivers/dms/getbits.cpp \
                     src/archivers/dms/maketbl.cpp \
@@ -159,12 +208,12 @@ LOCAL_SRC_FILES := src/archivers/7z/BraIA64.c \
                     src/osdep/picasso96.cpp \
                     src/osdep/writelog.cpp \
                     src/osdep/amiberry.cpp \
+                    src/osdep/ahi_v2.cpp \
                     src/osdep/amiberry_filesys.cpp \
                     src/osdep/amiberry_input.cpp \
                     src/osdep/amiberry_gfx.cpp \
                     src/osdep/amiberry_gui.cpp \
                     src/osdep/amiberry_mem.cpp \
-                    src/osdep/ahi_v2.cpp \
                     src/osdep/amiberry_whdbooter.cpp \
                     src/osdep/sigsegv_handler.cpp \
 					src/osdep/retroarch.cpp \
@@ -179,6 +228,7 @@ LOCAL_SRC_FILES := src/archivers/7z/BraIA64.c \
                     src/osdep/gui/CreateFilesysHardfile.cpp \
                     src/osdep/gui/EditFilesysVirtual.cpp \
                     src/osdep/gui/EditFilesysHardfile.cpp \
+                    src/osdep/gui/EditFilesysHardDrive.cpp \
                     src/osdep/gui/PanelAbout.cpp \
                     src/osdep/gui/PanelPaths.cpp \
                     src/osdep/gui/PanelQuickstart.cpp \
@@ -189,15 +239,17 @@ LOCAL_SRC_FILES := src/archivers/7z/BraIA64.c \
                     src/osdep/gui/PanelROM.cpp \
                     src/osdep/gui/PanelRAM.cpp \
                     src/osdep/gui/PanelFloppy.cpp \
-                    src/osdep/gui/PanelHD.cpp \
 					src/osdep/gui/PanelExpansions.cpp \
+                    src/osdep/gui/PanelHD.cpp \
                     src/osdep/gui/PanelRTG.cpp \
 					src/osdep/gui/PanelHWInfo.cpp \
                     src/osdep/gui/PanelInput.cpp \
                     src/osdep/gui/PanelIOPorts.cpp \
                     src/osdep/gui/PanelDisplay.cpp \
                     src/osdep/gui/PanelSound.cpp \
+                    src/osdep/gui/PanelDiskSwapper.cpp \
                     src/osdep/gui/PanelMisc.cpp \
+                    src/osdep/gui/PanelPrio.cpp \
                     src/osdep/gui/PanelSavestate.cpp \
                     src/osdep/gui/main_window.cpp \
                     src/osdep/gui/Navigation.cpp \
