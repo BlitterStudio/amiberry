87e9ed0ba7bed98a04f7b044e9465d8b701513f1
Dimitris Panokostas <midwan@gmail.com>
2022-02-21 17:12:21 +0100

    Make gencpu output code valid for both x86 and ARM

--- src/machdep/m68k.h
+++ src/machdep/m68k.h
@@ -31,7 +31,8 @@
   * Copyright 2004-2005 Richard Drummond
   */
 
-#if (defined(CPU_i386) && defined(X86_ASSEMBLY)) || (defined(CPU_x86_64) && defined(X86_64_ASSEMBLY))
+#pragma once
+#if defined(CPU_i386) || defined(CPU_x86_64)
 
 #ifndef REGS_DEFINED
 
@@ -39,8 +40,8 @@
   * Machine dependent structure for holding the 68k CCR flags
   */
 struct flag_struct {
-    unsigned int cznv;
-    unsigned int x;
+	unsigned int cznv;
+	unsigned int x;
 };
 
 
@@ -97,9 +98,9 @@ struct flag_struct {
  */
 STATIC_INLINE int cctrue (int cc)
 {
-    uae_u32 cznv = regs.ccrflags.cznv;
+	uae_u32 cznv = regs.ccrflags.cznv;
 
-    switch (cc) {
+	switch (cc) {
 	case 0:  return 1;								/*				T  */
 	case 1:  return 0;								/*				F  */
 	case 2:  return (cznv & (FLAGVAL_C | FLAGVAL_Z)) == 0;				/* !CFLG && !ZFLG		HI */
@@ -118,8 +119,8 @@ STATIC_INLINE int cctrue (int cc)
 		 return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & (FLAGVAL_N | FLAGVAL_Z)) == 0;
 	case 15: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);				/* ZFLG && (NFLG != VFLG)	LE */
 		 return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & (FLAGVAL_N | FLAGVAL_Z)) != 0;
-    }
-    return 0;
+	}
+	return 0;
 }
 
 #endif /* REGS_DEFINED */
@@ -163,30 +164,30 @@ struct flag_struct {
 
 STATIC_INLINE int cctrue(int cc)
 {
-    unsigned int nzcv = regs.ccrflags.nzcv;
-    switch(cc){
-     case 0: return 1;                       /* T */
-     case 1: return 0;                       /* F */
-     case 2: return (nzcv & 0x60000000) == 0; /* !GET_CFLG && !GET_ZFLG;  HI */
-     case 3: return (nzcv & 0x60000000) != 0; /* GET_CFLG || GET_ZFLG;    LS */
-     case 4: return (nzcv & 0x20000000) == 0; /* !GET_CFLG;               CC */
-     case 5: return (nzcv & 0x20000000) != 0; /* GET_CFLG;                CS */
-     case 6: return (nzcv & 0x40000000) == 0; /* !GET_ZFLG;               NE */
-     case 7: return (nzcv & 0x40000000) != 0; /* GET_ZFLG;                EQ */
-     case 8: return (nzcv & 0x10000000) == 0; /* !GET_VFLG;               VC */
-     case 9: return (nzcv & 0x10000000) != 0; /* GET_VFLG;                VS */
-     case 10:return (nzcv & 0x80000000) == 0; /* !GET_NFLG;               PL */
-     case 11:return (nzcv & 0x80000000) != 0; /* GET_NFLG;                MI */
-     case 12:return (((nzcv << 3) ^ nzcv) & 0x80000000) == 0; /* GET_NFLG == GET_VFLG;             GE */
-     case 13:return (((nzcv << 3) ^ nzcv) & 0x80000000) != 0; /* GET_NFLG != GET_VFLG;             LT */
-     case 14:
-        nzcv &= 0xd0000000;
-        return (((nzcv << 3) ^ nzcv) & 0xc0000000) == 0; /* !GET_ZFLG && (GET_NFLG == GET_VFLG);  GT */
-     case 15:
-        nzcv &= 0xd0000000;
-        return (((nzcv << 3) ^ nzcv) & 0xc0000000) != 0; /* GET_ZFLG || (GET_NFLG != GET_VFLG);   LE */
-    }
-    return 0;
+	unsigned int nzcv = regs.ccrflags.nzcv;
+	switch(cc){
+	 case 0: return 1;                       /* T */
+	 case 1: return 0;                       /* F */
+	 case 2: return (nzcv & 0x60000000) == 0; /* !GET_CFLG && !GET_ZFLG;  HI */
+	 case 3: return (nzcv & 0x60000000) != 0; /* GET_CFLG || GET_ZFLG;    LS */
+	 case 4: return (nzcv & 0x20000000) == 0; /* !GET_CFLG;               CC */
+	 case 5: return (nzcv & 0x20000000) != 0; /* GET_CFLG;                CS */
+	 case 6: return (nzcv & 0x40000000) == 0; /* !GET_ZFLG;               NE */
+	 case 7: return (nzcv & 0x40000000) != 0; /* GET_ZFLG;                EQ */
+	 case 8: return (nzcv & 0x10000000) == 0; /* !GET_VFLG;               VC */
+	 case 9: return (nzcv & 0x10000000) != 0; /* GET_VFLG;                VS */
+	 case 10:return (nzcv & 0x80000000) == 0; /* !GET_NFLG;               PL */
+	 case 11:return (nzcv & 0x80000000) != 0; /* GET_NFLG;                MI */
+	 case 12:return (((nzcv << 3) ^ nzcv) & 0x80000000) == 0; /* GET_NFLG == GET_VFLG;             GE */
+	 case 13:return (((nzcv << 3) ^ nzcv) & 0x80000000) != 0; /* GET_NFLG != GET_VFLG;             LT */
+	 case 14:
+		nzcv &= 0xd0000000;
+		return (((nzcv << 3) ^ nzcv) & 0xc0000000) == 0; /* !GET_ZFLG && (GET_NFLG == GET_VFLG);  GT */
+	 case 15:
+		nzcv &= 0xd0000000;
+		return (((nzcv << 3) ^ nzcv) & 0xc0000000) != 0; /* GET_ZFLG || (GET_NFLG != GET_VFLG);   LE */
+	}
+	return 0;
 }
 
 #endif /* REGS_DEFINED */
@@ -196,11 +197,11 @@ STATIC_INLINE int cctrue(int cc)
 #ifndef REGS_DEFINED
 
 struct flag_struct {
-    unsigned int c;
-    unsigned int z;
-    unsigned int n;
-    unsigned int v; 
-    unsigned int x;
+	unsigned int c;
+	unsigned int z;
+	unsigned int n;
+	unsigned int v; 
+	unsigned int x;
 };
 
 #else /* REGS_DEFINED */
@@ -234,25 +235,25 @@ struct flag_struct {
 
 STATIC_INLINE int cctrue(const int cc)
 {
-    switch(cc){
-     case 0: return 1;                       /* T */
-     case 1: return 0;                       /* F */
-     case 2: return !CFLG && !ZFLG;          /* HI */
-     case 3: return CFLG || ZFLG;            /* LS */
-     case 4: return !CFLG;                   /* CC */
-     case 5: return CFLG;                    /* CS */
-     case 6: return !ZFLG;                   /* NE */
-     case 7: return ZFLG;                    /* EQ */
-     case 8: return !VFLG;                   /* VC */
-     case 9: return VFLG;                    /* VS */
-     case 10:return !NFLG;                   /* PL */
-     case 11:return NFLG;                    /* MI */
-     case 12:return NFLG == VFLG;            /* GE */
-     case 13:return NFLG != VFLG;            /* LT */
-     case 14:return !ZFLG && (NFLG == VFLG); /* GT */
-     case 15:return ZFLG || (NFLG != VFLG);  /* LE */
-    }
-    return 0;
+	switch(cc){
+	 case 0: return 1;                       /* T */
+	 case 1: return 0;                       /* F */
+	 case 2: return !CFLG && !ZFLG;          /* HI */
+	 case 3: return CFLG || ZFLG;            /* LS */
+	 case 4: return !CFLG;                   /* CC */
+	 case 5: return CFLG;                    /* CS */
+	 case 6: return !ZFLG;                   /* NE */
+	 case 7: return ZFLG;                    /* EQ */
+	 case 8: return !VFLG;                   /* VC */
+	 case 9: return VFLG;                    /* VS */
+	 case 10:return !NFLG;                   /* PL */
+	 case 11:return NFLG;                    /* MI */
+	 case 12:return NFLG == VFLG;            /* GE */
+	 case 13:return NFLG != VFLG;            /* LT */
+	 case 14:return !ZFLG && (NFLG == VFLG); /* GT */
+	 case 15:return ZFLG || (NFLG != VFLG);  /* LE */
+	}
+	return 0;
 }
 
 #endif /* REGS_DEFINED */
