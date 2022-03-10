bc4ad03a6dbf932b4de68ca917a15b1fa3d7719b
Dimitris Panokostas <midwan@gmail.com>
2022-03-10 20:04:51 +0100

    WIP: Added vkbd images and updated code
    Not finished yet, WIP

--- src/osdep/vkbd/vkbd.h
+++ src/osdep/vkbd/vkbd.h
@@ -1,16 +1,11 @@
 #ifndef VKBD_H
 #define VKBD_H
-#include<SDL.h>
-
-#ifdef USE_SDL2
-#include "sdl2_to_sdl1.h"
-#endif
 
 #define VKBD_X 20
 #define VKBD_Y 200
 
 #define VKBD_LEFT 1
-#define VKBD_RIGHT 2	
+#define VKBD_RIGHT 2
 #define VKBD_UP 4
 #define VKBD_DOWN 8
 #define VKBD_BUTTON 16
@@ -20,8 +15,8 @@
 #define VLBD_BUTTON2 128
 
 // special return codes for vkbd_process
-#define KEYCODE_NOTHING -1234567
-#define KEYCODE_STICKY_RESET -100
+#define KEYCODE_NOTHING (-1234567)
+#define KEYCODE_STICKY_RESET (-100)
 
 #define NUM_STICKY 7 // number of sticky keys (shift, alt etc)
 
@@ -29,7 +24,6 @@ int vkbd_init(void);
 void vkbd_quit(void);
 void vkbd_redraw(void);
 int vkbd_process(void);
-void vkbd_init_button2(void);
 void vkbd_displace_up(void);
 void vkbd_displace_down(void);
 void vkbd_transparency_up(void);
@@ -38,16 +32,16 @@ void vkbd_reset_sticky_keys(void);
 
 extern int vkbd_mode;
 extern int vkbd_move;
-typedef struct
+
+using t_vkbd_sticky_key = struct
 {
 	int code; // amiga-side keycode
 	bool stuck; // is it currently stuck pressed?
 	bool can_switch; // de-bounce
 	unsigned char index; // index in vkbd_rect[]
-} t_vkbd_sticky_key;
+};
 extern t_vkbd_sticky_key vkbd_sticky_key[NUM_STICKY];
 extern int vkbd_key;
 extern int vkbd_keysave;
-extern SDLKey vkbd_button2;
 extern int keymappings[10][3];
 #endif // VKBD_H
