5bda64435aa706771ed1c19ac042390bdf660635
Dimitris Panokostas <midwan@gmail.com>
2022-03-17 14:02:03 +0100

    Updated beta version date

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@
 #include "fsdb_host.h"
 #include "keyboard.h"
 
-static const char __ver[40] = "$VER: Amiberry beta (2022-03-07)";
+static const char __ver[40] = "$VER: Amiberry beta (2022-03-17)";
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
 struct uae_prefs currprefs, changed_prefs;
--- src/osdep/target.h
+++ src/osdep/target.h
@@ -24,8 +24,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry beta (2022-03-07)")
-#define AMIBERRYDATE MAKEBD(2022, 3, 7)
+#define AMIBERRYVERSION _T("Amiberry beta (2022-03-17)")
+#define AMIBERRYDATE MAKEBD(2022, 3, 17)
 
 #define IHF_WINDOWHIDDEN 6
 
