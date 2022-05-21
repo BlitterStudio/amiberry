98b052cfc03129ad18c9a0136a5fe51942c591d5
Dimitris Panokostas <midwan@gmail.com>
2022-05-21 22:04:37 +0200

    Added release on new version tag in workflow

---
+++ .github/tag-changelog.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    types: [
+      { types: ["feat", "feature"], label: "🎉 New Features" },
+      { types: ["fix", "bugfix", "bug"], label: "🐛 Bugfixes" },
+      { types: ["improvements", "enhancement"], label: "🔨 Improvements" },
+      { types: ["perf"], label: "🏎️ Performance Improvements" },
+      { types: ["build", "ci"], label: "🏗️ Build System" },
+      { types: ["refactor"], label: "🪚 Refactors" },
+      { types: ["doc", "docs"], label: "📚 Documentation Changes" },
+      { types: ["test", "tests"], label: "🔍 Tests" },
+      { types: ["style"], label: "💅 Code Style Changes" },
+      { types: ["chore"], label: "🧹 Chores" },
+      { types: ["other"], label: "Other Changes" },
+    ],
+  
+    // excludeTypes: ["other"],
+  
+    renderTypeSection: function (label, commits) {
+      let text = `\n## ${label}\n`;
+  
+      commits.forEach((commit) => {
+        text += `- ${commit.subject}\n`;
+      });
+  
+      return text;
+    },
+  
+    renderChangelog: function (release, changes) {
+      const now = new Date();
+      return `# ${release} - ${now.toISOString().substr(0, 10)}\n` + changes + "\n\n";
+    },
+  };
