04fdb8e9880a70537c46baeefdd9ce8285d040a9
Dimitris Panokostas <dimitris.panokostas@norce.io>
2022-12-25 19:19:56 +0100

    Updated version date

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -24,8 +24,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry DEV beta (2022-12-20)")
-#define AMIBERRYDATE MAKEBD(2022, 12, 20)
+#define AMIBERRYVERSION _T("Amiberry DEV (2022-12-25)")
+#define AMIBERRYDATE MAKEBD(2022, 12, 25)
 
 #define IHF_WINDOWHIDDEN 6
 
