7e3c09c0fd129727250befe4aad09468d70a9375
Dimitris Panokostas <midwan@gmail.com>
2023-01-02 21:39:04 +0100

    Updated version date
    Synced with WinUAE 41010b2

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@
 #include "fsdb_host.h"
 #include "keyboard.h"
 
-static const char __ver[40] = "$VER: Amiberry NG beta (2022-12-31)";
+static const char __ver[40] = "$VER: Amiberry NG beta (2023-01-02)";
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
 struct uae_prefs currprefs, changed_prefs;
