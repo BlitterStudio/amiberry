cb4c629b9fb3ae93129430f287947de5a8c0d3e0
Dimitris Panokostas <midwan@gmail.com>
2023-02-11 19:24:01 +0100

    bugfix: fixed compile would fail after previous commit

--- src/include/serial.h
+++ src/include/serial.h
@@ -32,6 +32,7 @@ extern int doreadser, serstat;
 extern void serial_flush_buffer (void);
 
 extern void serial_hsynchandler(void);
+extern void serial_check_irq(void);
 extern void serial_rethink(void);
 
 extern int uaeser_getdatalength (void);
