c7e9bd4bf8732d95e87d5623e295d1ff7ca62fba
Dimitris Panokostas <midwan@gmail.com>
2023-02-20 15:23:44 +0100

    ci: Set a version string, so that macOS can sign the binary

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@
 #include "fsdb_host.h"
 #include "keyboard.h"
 
-static const char __ver[40] = "$VER: Amiberry NG beta (2023-02-19)";
+static const char __ver[40] = "$VER: Amiberry 6.0 NG beta (2023-02-19)";
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
 struct uae_prefs currprefs, changed_prefs;
