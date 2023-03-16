33b40f7adc627f9e54a3d25c9ab857cb31c160dd
Dimitris Panokostas <midwan@gmail.com>
2023-03-16 09:53:34 +0100

    Removed obsolete gencpu.sh file

--- src/gencpu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ `sdl2-config --cflags` -I./ -I./include -I./osdep -I./threaddep -I./archivers -DAMIBERRY -D_FILE_OFFSET_BITS=64 -lSDL2 -fuse-ld=gold -o gencpu cpudefs.cpp gencpu.cpp readcpu.cpp ./osdep/charset.cpp
