7440cc298c7f974e56e3a87795adaf2bde242649
Dimitris Panokostas <midwan@gmail.com>
2023-10-28 23:51:55 +0200

    enhancement: added P96 ModeChanged checks

--- src/osdep/picasso96.h
+++ src/osdep/picasso96.h
@@ -640,6 +640,7 @@ struct picasso96_state_struct
 	long		XYOffset;
 	bool        dualclut, advDragging;
 	int         HLineDBL, VLineDBL;
+    bool        ModeChanged;
 };
 
 extern void InitPicasso96(int monid);
