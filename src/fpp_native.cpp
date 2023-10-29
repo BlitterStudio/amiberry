b4e0d75886b59d79644e0d9b0075055ae76955e8
Dimitris Panokostas <midwan@gmail.com>
2023-10-29 19:04:32 +0100

    bugfix: FIx FABS(-0)

--- src/fpp_native.cpp
+++ src/fpp_native.cpp
@@ -892,7 +892,7 @@ static void fp_log2(fpdata *a, fpdata *b)
 static void fp_abs(fpdata *a, fpdata *b, int prec)
 {
 	fp_set_prec(prec);
-	a->fp = b->fp < 0.0 ? -b->fp : b->fp;
+    a->fp = abs(b->fp);
 	fp_reset_prec(a);
 }
 static void fp_cosh(fpdata *a, fpdata *b)
