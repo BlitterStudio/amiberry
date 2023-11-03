b4f1bd95078c79a5c6dd14c816f34f91ba363a1c
Dimitris Panokostas <midwan@gmail.com>
2023-11-03 22:25:37 +0100

    enhancement: check for usage help cmd line parameter early
    Check for the -h or --help parameter early, before initializing SDL2. This allows the print of the usage text output even in environments where no display hardware exists, and the GUI would normally fail to show.

--- src/osdep/amiberry.cpp
+++ src/osdep/amiberry.cpp
@@ -3579,6 +3579,11 @@ bool data_dir_exists(char* directory)
 
 int main(int argc, char* argv[])
 {
+    for (auto i = 1; i < argc; i++) {
+        if (_tcscmp(argv[i], _T("-h")) == 0 || _tcscmp(argv[i], _T("--help")) == 0)
+            usage();
+    }
+
 	struct sigaction action{};
 
 	max_uae_width = 8192;
