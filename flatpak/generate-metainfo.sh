5a2a02d72bc8e622fbea8b83d9b5d5b13a5c8380
Dimitris Panokostas <midwan@gmail.com>
2023-11-09 18:35:02 +0100

    ci: flatpak improvements
    Generate a Preview release with the Amiberry flatpak automatically

---
+++ flatpak/generate-metainfo.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+
+if [[ $# -lt 1 ]]; then
+	echo "Output directory must be provided as a parameter"
+	exit 1
+fi
+
+APPID="com.blitterstudio.amiberry"
+OUTDIR=$(realpath "$1")
+OUTFILE="${OUTDIR}/${APPID}.metainfo.xml"
+
+pushd "${SCRIPTDIR}" >/dev/null || exit
+GIT_DATE=$(git log -1 --pretty=%cd --date=short)
+GIT_HASH=$(git rev-parse HEAD)
+GIT_VERSION=$(git describe --dirty | tr -d '\r\n')
+if [[ "${GIT_VERSION}" == "" ]]; then
+	GIT_VERSION=$(git rev-parse HEAD)
+fi
+
+popd >/dev/null || exit
+
+echo "GIT_DATE: ${GIT_DATE}"
+echo "GIT_VERSION: ${GIT_VERSION}"
+echo "GIT_HASH: ${GIT_HASH}"
+
+cp "${SCRIPTDIR}/${APPID}.metainfo.xml.in" "${OUTFILE}"
+
+sed -i -e "s/@GIT_VERSION@/${GIT_VERSION}/" "${OUTFILE}"
+sed -i -e "s/@GIT_DATE@/${GIT_DATE}/" "${OUTFILE}"
+sed -i -e "s/@GIT_HASH@/${GIT_HASH}/" "${OUTFILE}"
