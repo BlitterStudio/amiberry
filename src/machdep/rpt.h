93a5008f9fb0d74550c6ca81b97035c14c87430e
Dimitris Panokostas <midwan@gmail.com>
2023-11-27 21:31:48 +0100

    refactor: use SDL2 perf counter instead of clock_gettime()
    No measurable performance difference, but it simplifies the code a lot

--- src/machdep/rpt.h
+++ src/machdep/rpt.h
@@ -9,56 +9,17 @@
 #ifndef _RPT_H_
 #define _RPT_H_
 
-#if defined(__PSP2__) // NOT __SWITCH__
-#include <psp2/kernel/processmgr.h>
-#endif
-
-#if defined(__SWITCH__)
-#include <switch.h>
-#endif
-
 /* frame_time_t is often cast to int in the code so we use int for now... */
 typedef uae_s64 uae_time_t;
 
-void uae_time_calibrate(void);
+void uae_time_calibrate();
 typedef uae_time_t frame_time_t;
 
 extern int64_t g_uae_epoch;
-
 /* Returns elapsed time in microseconds since start of emulator. */
-static __inline__ frame_time_t read_processor_time (void)
+static __inline__ frame_time_t read_processor_time()
 {
-  int64_t time;
-#if defined(__PSP2__) // NOT __SWITCH__
-  time = sceKernelGetProcessTimeWide();
-#elif defined(__SWITCH__)
-time = (int64_t) ((svcGetSystemTick() * 1000000) / 19200000);
-#else
-  struct timespec ts{};
-
-  clock_gettime (CLOCK_MONOTONIC, &ts);
-
-  time = (int64_t) ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
-#endif
-  return time - g_uae_epoch;
-}
-
-
-static __inline__ int64_t read_processor_time_ns (void)
-{
-  int64_t time;
-#if defined(__PSP2__) // NOT __SWITCH__
-  time = sceKernelGetProcessTimeWide();
-#elif defined(__SWITCH__)
-time = (int64_t) ((svcGetSystemTick() * 1000000) / 19200000);
-#else
-  struct timespec ts{};
-  
-  clock_gettime (CLOCK_MONOTONIC, &ts);
-
-  time = (int64_t) ts.tv_sec * 1000000000LL + ts.tv_nsec;
-#endif
-  return time;
+	return frame_time_t((SDL_GetPerformanceCounter() / 1000) - g_uae_epoch);
 }
 
 #endif /* _RPT_H_ */
