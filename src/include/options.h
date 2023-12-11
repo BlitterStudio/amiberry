d50d9df632dc895c3efdb26af9cfe8060b6ad017
Dimitris Panokostas <midwan@gmail.com>
2023-12-12 00:39:10 +0100

    bump version in Options also

--- src/include/options.h
+++ src/include/options.h
@@ -17,8 +17,8 @@
 #include "guisan/color.hpp"
 
 #define UAEMAJOR 6
-#define UAEMINOR 0
-#define UAESUBREV 1
+#define UAEMINOR 1
+#define UAESUBREV 0
 
 #define MAX_AMIGADISPLAYS 1
 
--- src/machdep/m68k.h
+++ src/machdep/m68k.h
@@ -24,11 +24,11 @@
  */
  /* 
   * UAE - The Un*x Amiga Emulator
-  * 
+  *
   * MC68000 emulation - machine dependent bits
   *
   * Copyright 1996 Bernd Schmidt
-  *
+  * Copyright 2004-2005 Richard Drummond
   */
 
 #ifndef M68K_FLAGS_H
@@ -230,12 +230,12 @@ static inline int cctrue(int cc)
 
 #else /* !SAHF_SETO_PROFITABLE */
 
-  /*
-   * Machine dependent structure for holding the 68k CCR flags
-   */
+/*
+ * Machine dependent structure for holding the 68k CCR flags
+ */
 struct flag_struct {
-    uae_u32 cznv;
-    uae_u32 x;
+	uae_u32 cznv;
+	uae_u32 x;
 };
 
 extern struct flag_struct regflags __asm__ ("regflags");
@@ -291,39 +291,39 @@ extern struct flag_struct regflags __asm__ ("regflags");
  */
 static inline int cctrue(int cc)
 {
-    uae_u32 cznv = regflags.cznv;
+	uae_u32 cznv = regflags.cznv;
 
-    switch (cc) {
-    case 0:  return 1;                              /*              T  */
-    case 1:  return 0;                              /*              F  */
-    case 2:  return (cznv & (FLAGVAL_C | FLAGVAL_Z)) == 0;              /* !CFLG && !ZFLG       HI */
-    case 3:  return (cznv & (FLAGVAL_C | FLAGVAL_Z)) != 0;              /*  CFLG || ZFLG        LS */
-    case 4:  return (cznv & FLAGVAL_C) == 0;                    /* !CFLG            CC */
-    case 5:  return (cznv & FLAGVAL_C) != 0;                    /*  CFLG            CS */
-    case 6:  return (cznv & FLAGVAL_Z) == 0;                    /* !ZFLG            NE */
-    case 7:  return (cznv & FLAGVAL_Z) != 0;                    /*  ZFLG            EQ */
-    case 8:  return (cznv & FLAGVAL_V) == 0;                    /* !VFLG            VC */
-    case 9:  return (cznv & FLAGVAL_V) != 0;                    /*  VFLG            VS */
-    case 10: return (cznv & FLAGVAL_N) == 0;                    /* !NFLG            PL */
-    case 11: return (cznv & FLAGVAL_N) != 0;                    /*  NFLG            MI */
+	switch (cc) {
+		case 0:  return 1;                              /*              T  */
+		case 1:  return 0;                              /*              F  */
+		case 2:  return (cznv & (FLAGVAL_C | FLAGVAL_Z)) == 0;              /* !CFLG && !ZFLG       HI */
+		case 3:  return (cznv & (FLAGVAL_C | FLAGVAL_Z)) != 0;              /*  CFLG || ZFLG        LS */
+		case 4:  return (cznv & FLAGVAL_C) == 0;                    /* !CFLG            CC */
+		case 5:  return (cznv & FLAGVAL_C) != 0;                    /*  CFLG            CS */
+		case 6:  return (cznv & FLAGVAL_Z) == 0;                    /* !ZFLG            NE */
+		case 7:  return (cznv & FLAGVAL_Z) != 0;                    /*  ZFLG            EQ */
+		case 8:  return (cznv & FLAGVAL_V) == 0;                    /* !VFLG            VC */
+		case 9:  return (cznv & FLAGVAL_V) != 0;                    /*  VFLG            VS */
+		case 10: return (cznv & FLAGVAL_N) == 0;                    /* !NFLG            PL */
+		case 11: return (cznv & FLAGVAL_N) != 0;                    /*  NFLG            MI */
 #if FLAGBIT_N > FLAGBIT_V
-    case 12: return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & FLAGVAL_N) == 0;  /*  NFLG == VFLG        GE */
-    case 13: return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & FLAGVAL_N) != 0;  /*  NFLG != VFLG        LT */
-    case 14: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* !ZFLG && (NFLG == VFLG)   GT */
-         return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & (FLAGVAL_N | FLAGVAL_Z)) == 0;
-    case 15: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* ZFLG || (NFLG != VFLG)   LE */
-         return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & (FLAGVAL_N | FLAGVAL_Z)) != 0;
+		case 12: return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & FLAGVAL_N) == 0;  /*  NFLG == VFLG        GE */
+		case 13: return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & FLAGVAL_N) != 0;  /*  NFLG != VFLG        LT */
+		case 14: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* !ZFLG && (NFLG == VFLG)   GT */
+			return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & (FLAGVAL_N | FLAGVAL_Z)) == 0;
+		case 15: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* ZFLG || (NFLG != VFLG)   LE */
+			return (((cznv << (FLAGBIT_N - FLAGBIT_V)) ^ cznv) & (FLAGVAL_N | FLAGVAL_Z)) != 0;
 #else
-    case 12: return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & FLAGVAL_V) == 0;  /*  NFLG == VFLG        GE */
-    case 13: return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & FLAGVAL_V) != 0;  /*  NFLG != VFLG        LT */
-    case 14: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* !ZFLG && (NFLG == VFLG)   GT */
-         return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & (FLAGVAL_V | FLAGVAL_Z)) == 0;
-    case 15: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* ZFLG || (NFLG != VFLG)   LE */
-         return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & (FLAGVAL_V | FLAGVAL_Z)) != 0;
+		case 12: return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & FLAGVAL_V) == 0;  /*  NFLG == VFLG        GE */
+		case 13: return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & FLAGVAL_V) != 0;  /*  NFLG != VFLG        LT */
+		case 14: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* !ZFLG && (NFLG == VFLG)   GT */
+			return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & (FLAGVAL_V | FLAGVAL_Z)) == 0;
+		case 15: cznv &= (FLAGVAL_N | FLAGVAL_Z | FLAGVAL_V);               /* ZFLG || (NFLG != VFLG)   LE */
+			return (((cznv << (FLAGBIT_V - FLAGBIT_N)) ^ cznv) & (FLAGVAL_V | FLAGVAL_Z)) != 0;
 #endif
-    }
-    abort ();
-    return 0;
+	}
+	abort ();
+	return 0;
 }
 
 
