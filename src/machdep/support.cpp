757dc354b84f33421f663e7f37be879713161a75
Dimitris Panokostas <midwan@gmail.com>
2023-12-21 22:05:32 +0100

    refactor: use SDL2 functions for calibrating and counting time

--- src/machdep/support.cpp
+++ src/machdep/support.cpp
@@ -5,11 +5,33 @@
 #include "custom.h"
 #include "xwin.h"
 
-int64_t g_uae_epoch = 0;
+static int qpcdivisor = 0;
 
-void uae_time_calibrate(void)
+/* Returns elapsed time in microseconds since start of emulator. */
+uae_time_t uae_time()
+{
+	frame_time_t t;
+	auto counter = SDL_GetPerformanceCounter();
+	if (qpcdivisor == 0)
+		t = static_cast<frame_time_t>(counter);
+	else
+		t = static_cast<frame_time_t>(counter >> qpcdivisor);
+	if (!t)
+		t++;
+	return t;
+}
+
+void uae_time_calibrate()
 {
 	// Initialize timebase
-	g_uae_epoch = read_processor_time();
-	syncbase = 1000000; // Microseconds
+	auto freq = SDL_GetPerformanceFrequency();
+	// limit to 10MHz
+	qpcdivisor = 0;
+	while (freq > 10000000)
+	{
+		freq >>= 1;
+		qpcdivisor++;
+	}
+
+	syncbase = static_cast<frame_time_t>(freq);
 }
