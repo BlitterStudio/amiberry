cf08642d9ec8505406aada4c0985792d5495a450
Dimitris Panokostas <midwan@gmail.com>
2023-12-21 22:42:50 +0100

    refactor: use SDL2 functions for calibrating and counting time (#1209)
    * refactor: use SDL2 functions for calibrating and counting time
    
    * cleanup

--- src/machdep/support.cpp
+++ src/machdep/support.cpp
@@ -5,11 +5,31 @@
 #include "custom.h"
 #include "xwin.h"
 
-int64_t g_uae_epoch = 0;
+static int qpcdivisor = 0;
 
-void uae_time_calibrate(void)
+uae_time_t uae_time()
 {
-	// Initialize timebase
-	g_uae_epoch = read_processor_time();
-	syncbase = 1000000; // Microseconds
+	frame_time_t t;
+	const auto counter = SDL_GetPerformanceCounter();
+	if (qpcdivisor == 0)
+		t = static_cast<frame_time_t>(counter);
+	else
+		t = static_cast<frame_time_t>(counter >> qpcdivisor);
+	if (!t)
+		t++;
+	return t;
+}
+
+void uae_time_calibrate()
+{
+	auto freq = SDL_GetPerformanceFrequency();
+	// limit to 10MHz
+	qpcdivisor = 0;
+	while (freq > 10000000)
+	{
+		freq >>= 1;
+		qpcdivisor++;
+	}
+
+	syncbase = static_cast<frame_time_t>(freq);
 }
