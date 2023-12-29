e3bc73a82ff718cf39baf1ac276c10edb47ac646
Dimitris Panokostas <midwan@gmail.com>
2023-12-29 12:49:07 +0100

    Updated version date

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,7 @@
 #include "fsdb_host.h"
 #include "keyboard.h"
 
-static const char __ver[40] = "$VER: Amiberry 5.6.6 (2023-12-26)";
+static const char __ver[40] = "$VER: Amiberry 5.6.6 (2023-12-29)";
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
 struct uae_prefs currprefs, changed_prefs;
