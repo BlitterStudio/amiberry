efda1f786f0eca5d2f907cb7944198e63e9e0aa1
Dimitris Panokostas <midwan@gmail.com>
2023-12-29 11:31:56 +0100

    refactor: do not hardcode path and filename for capsimg library

--- src/include/uae/uae.h
+++ src/include/uae/uae.h
@@ -7,7 +7,4 @@ char* uae_clipboard_get_text(void);
 void uae_clipboard_free_text(char* text);
 void uae_clipboard_put_text(const char* text);
 
-typedef const char * (*amiga_plugin_lookup_function)(const char *name);
-void amiga_set_plugin_lookup_function(amiga_plugin_lookup_function function);
-
 #endif // LIBAMIGA_LIBAMIGA_H_
