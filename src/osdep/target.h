f397e21a4ec63bc00c147a2a150955d7c9bd85cd
Dimitris Panokostas <midwan@gmail.com>
2024-03-20 13:36:18 +0100

    Updated version date

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -20,8 +20,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry v5.6.9 (2024-03-18)")
-#define AMIBERRYDATE MAKEBD(2024, 3, 18)
+#define AMIBERRYVERSION _T("Amiberry v5.6.9 (2024-03-20)")
+#define AMIBERRYDATE MAKEBD(2024, 3, 20)
 #define COPYRIGHT _T("Copyright (C) 2016-2024 Dimitris Panokostas")
 
 #define IHF_WINDOWHIDDEN 6
