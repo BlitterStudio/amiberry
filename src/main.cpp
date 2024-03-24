0d315885cb58e85c080b8e994395a4476f5d8a20
Dimitris Panokostas <midwan@gmail.com>
2024-03-24 16:24:58 +0100

    Bumped version date

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,7 @@
 #include "fsdb_host.h"
 #include "keyboard.h"
 
-static const char __ver[40] = "$VER: Amiberry 5.6.9 (2024-03-21)";
+static const char __ver[40] = "$VER: Amiberry 5.7.0 (2024-03-24)";
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
 struct uae_prefs currprefs, changed_prefs;
