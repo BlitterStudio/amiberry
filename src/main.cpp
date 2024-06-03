c3891e6011ff91257f09f2389a794833e737f49b
Dimitris Panokostas <midwan@gmail.com>
2024-06-03 19:40:24 +0200

    Updated version date

--- src/main.cpp
+++ src/main.cpp
@@ -75,7 +75,7 @@
 #include "fsdb_host.h"
 #include "keyboard.h"
 
-static const char __ver[40] = "$VER: Amiberry 5.7.1 (2024-05-24)";
+static const char __ver[40] = "$VER: Amiberry 5.7.1 (2024-06-03)";
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
 extern int console_logging;
--- src/osdep/target.h
+++ src/osdep/target.h
@@ -20,8 +20,8 @@
 #define GETBDM(x) (((x) - (((x) / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define AMIBERRYVERSION _T("Amiberry v5.7.1 (2024-05-24)")
-#define AMIBERRYDATE MAKEBD(2024, 5, 24)
+#define AMIBERRYVERSION _T("Amiberry v5.7.1 (2024-06-03)")
+#define AMIBERRYDATE MAKEBD(2024, 6, 3)
 #define COPYRIGHT _T("Copyright (C) 2016-2024 Dimitris Panokostas")
 
 #define IHF_WINDOWHIDDEN 6
