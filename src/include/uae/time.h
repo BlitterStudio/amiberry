7258679896cfa1d041de0de768ca6fe902a12827
Dimitris Panokostas <midwan@gmail.com>
2024-09-15 12:55:27 +0200

    chore: sync time/rpt headers with WinUAE

--- src/include/uae/time.h
+++ src/include/uae/time.h
@@ -2,13 +2,12 @@
 #define UAE_TIME_H
 
 #include "uae/types.h"
-#include "machdep/rpt.h"
 
 /* frame_time_t is often cast to int in the code so we use int for now... */
 typedef uae_s64 uae_time_t;
 
 void uae_time_init(void);
-//void uae_time_calibrate(void);
+void uae_time_calibrate(void);
 uae_time_t uae_time(void);
 
 #ifdef _WIN32
@@ -19,10 +18,17 @@ uae_s64 read_processor_time_rdtsc(void);
 
 typedef uae_time_t frame_time_t;
 
-//static inline frame_time_t read_processor_time(void)
-//{
-//	return uae_time();
-//}
+extern int64_t g_uae_epoch;
+
+/* Returns elapsed time in microseconds since start of emulator. */
+static inline frame_time_t read_processor_time(void)
+{
+	int64_t time;
+	struct timespec ts {};
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	time = ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
+	return time - g_uae_epoch;
+}
 
 extern frame_time_t syncbase;
 
