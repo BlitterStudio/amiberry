38530178a8848f8c689e20b0a60ff59c41a2e004
Solskogen <christer.solskogen@gmail.com>
2024-09-20 12:02:21 +0200

    First imports of toolchain files for cross-compiling

---
+++ cmake/Toolchain-arm-linux-gnueabihf.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_CROSSCOMPILING TRUE)
+
