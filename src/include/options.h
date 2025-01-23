231468d6b5996d63be3c3d75b283b1f40ff23895
Dimitris Panokostas <midwan@gmail.com>
2025-01-23 13:20:42 +0100

    Bump version

--- src/include/options.h
+++ src/include/options.h
@@ -20,7 +20,7 @@
 
 #define UAEMAJOR 7
 #define UAEMINOR 0
-#define UAESUBREV 1
+#define UAESUBREV 2
 
 #define MAX_AMIGADISPLAYS 1
 
--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@
 #include "keyboard.h"
 
 // Special version string so that AmigaOS can detect it
-static constexpr char __ver[40] = "$VER: Amiberry v7.0 (2025-01-14)";
+static constexpr char __ver[40] = "$VER: Amiberry v7.0 (2025-01-23)";
 
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
