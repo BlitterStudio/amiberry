c0db52fa7755490dbd1a28102055a0214412ad5f
Dimitris Panokostas <midwan@gmail.com>
2025-01-23 13:43:20 +0100

    refactor: cdda_play should be int

--- src/osdep/cda_play.h
+++ src/osdep/cda_play.h
@@ -53,6 +53,6 @@ struct cda_play
 	cda_play_read_block read_block;
 };
 
-void ciw_cdda_play(void* ciw);
+int ciw_cdda_play(void* ciw);
 void ciw_cdda_stop(struct cda_play* ciw);
 int ciw_cdda_setstate(struct cda_play* ciw, int state, int playpos);
