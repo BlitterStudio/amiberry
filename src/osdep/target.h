135d230c5514a7aa680536dbad27c98758446bf2
Dimitris Panokostas <midwan@gmail.com>
2025-03-30 20:14:27 +0200

    Update version date

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -28,7 +28,7 @@
 #define AMIBERRYBETA _T("")
 #endif
 
-#define AMIBERRYDATE MAKEBD(2025, 3, 21)
+#define AMIBERRYDATE MAKEBD(2025, 3, 30)
 #define COPYRIGHT _T("Copyright (C) 2025 Dimitris Panokostas")
 
 #ifndef AMIBERRYEXTRA
