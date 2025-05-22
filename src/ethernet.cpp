654e0571b78e6f9002bb6ad05c40464adac6226b
Dimitris Panokostas <midwan@gmail.com>
2025-05-22 18:04:46 +0200

    feat: implement support for uaenet.device using pcap library (#1487)
    First changes needed to implement support for uaenet.device - still untested, but it builds and runs without issues.
    
    This adds one more requirement to build: libpcap

--- src/ethernet.cpp
+++ src/ethernet.cpp
@@ -5,6 +5,19 @@
 #include "ethernet.h"
 #ifdef _WIN32
 #include "win32_uaenet.h"
+#else
+typedef void (uaenet_gotfunc)(void *dev, const uae_u8 *data, int len);
+typedef int (uaenet_getfunc)(void *dev, uae_u8 *d, int *len);
+
+extern struct netdriverdata *uaenet_enumerate (const TCHAR *name);
+extern void uaenet_enumerate_free (void);
+extern void uaenet_close_driver (struct netdriverdata *tc);
+
+extern int uaenet_getdatalenght (void);
+extern int uaenet_getbytespending (void*);
+extern int uaenet_open (void*, struct netdriverdata*, void*, uaenet_gotfunc*, uaenet_getfunc*, int, const uae_u8*);
+extern void uaenet_close (void*);
+extern void uaenet_trigger (void*);
 #endif
 #include "threaddep/thread.h"
 #include "options.h"
