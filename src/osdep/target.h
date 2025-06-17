707af38f8c94e6754c15cccee9e1f031a0914747
Dimitris Panokostas <midwan@gmail.com>
2025-06-17 15:04:19 +0200

    Bump version to 7.1.0 for next release

--- src/osdep/target.h
+++ src/osdep/target.h
@@ -28,7 +28,7 @@
 #define AMIBERRYBETA _T("")
 #endif
 
-#define AMIBERRYDATE MAKEBD(2025, 6, 13)
+#define AMIBERRYDATE MAKEBD(2025, 6, 17)
 #define COPYRIGHT _T("Copyright (C) 2025 Dimitris Panokostas")
 
 #ifndef AMIBERRYEXTRA
