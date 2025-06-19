d6fb2be80d0bac8f2aca2d02ec0866fafadd6e3e
Dimitris Panokostas <midwan@gmail.com>
2025-06-19 16:33:31 +0200

    ci: improve aarch64 toolchain file

--- cmake/Toolchain-aarch64-linux-gnu.cmake
+++ cmake/Toolchain-aarch64-linux-gnu.cmake
@@ -1,6 +1,21 @@
 set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_ASM_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_STRIP aarch64-linux-gnu-strip)
+
+# Adjust sysroot if needed (Fedora cross tools may provide one)
+# set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
+
+# Set search paths for libraries and includes
+set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
 set(CMAKE_CROSSCOMPILING TRUE)
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE arm64)
