7440b95e416603fd4142df9acd36bd94512ba48a
Dimitris Panokostas <midwan@gmail.com>
2025-08-23 22:58:08 +0200

    enhancement: Support broken cue files with "TRACK 0" instead of "TRACK 01". (fixes #1691)

--- src/blkdev_cdimage.cpp
+++ src/blkdev_cdimage.cpp
@@ -1756,7 +1756,13 @@ static int parsecue (struct cdunit *cdu, struct zfile *zcue, const TCHAR *img, c
 			index0 = -1;
 			lastpregap = 0;
 			lastpostgap = 0;
+			skipspace(&p);
+			TCHAR *pt = p;
 			tracknum = _tstoi (nextstring (&p));
+			// fix broken "TRACK 0"
+			if (tracknum == 0 && pt[0] == '0' && pt[1] == 0 && cdu->tracks == 0) {
+				tracknum = 1;
+			}
 			tracktype = nextstring (&p);
 			if (!tracktype)
 				break;
@@ -1783,7 +1789,9 @@ static int parsecue (struct cdunit *cdu, struct zfile *zcue, const TCHAR *img, c
 
 				if (tracknum > 1 && newfile) {
 					t--;
-					secoffset += (int)(t->filesize / t->size);
+					if (t->size) {
+						secoffset += (int)(t->filesize / t->size);
+					}
 					t++;
 				}
 
