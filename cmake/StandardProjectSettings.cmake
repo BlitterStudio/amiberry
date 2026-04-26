ac60b17135b3b387219f6279b2ebba13626582ba
polyduekes-git <polyduekes@proton.me>
2026-04-26 18:37:43 +0530

    Fix WITH_LTO cmake option for FreeBSD+Clang

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -83,6 +83,13 @@ if(WITH_OPTIMIZE)
 endif()
 
 if(WITH_LTO)
+	# ensure LTO links with lld on FreeBSD/clang.
+    if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD" AND CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        string(APPEND CMAKE_EXE_LINKER_FLAGS " -fuse-ld=lld")
+        string(APPEND CMAKE_SHARED_LINKER_FLAGS " -fuse-ld=lld")
+        string(APPEND CMAKE_MODULE_LINKER_FLAGS " -fuse-ld=lld")
+    endif()
+
     include(CheckIPOSupported)
     check_ipo_supported(RESULT lto_supported OUTPUT lto_error)
     if(lto_supported)
