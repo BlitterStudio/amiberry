832b4d097ddfbeb844ad64c94a635862c1f967b3
Dimitris Panokostas <midwan@gmail.com>
2026-05-21 14:02:39 +0200

    Harden ARM64 JIT architecture detection

--- src/jit/compemu.cpp
+++ src/jit/compemu.cpp
@@ -2,7 +2,8 @@
 // Created by midwan on 22/12/2023.
 //
 
-#if defined(CPU_arm) || defined(CPU_AARCH64)
+#if defined(CPU_arm) || defined(CPU_AARCH64) || defined(__arm__) || defined(_M_ARM) || \
+	defined(__aarch64__) || defined(_M_ARM64) || defined(_M_ARM64EC)
 #include "arm/compemu_arm.cpp"
 #elif defined(__x86_64__) || defined(_M_AMD64)
 #include "x86/compemu_x86.cpp"
--- src/jit/compemu.h
+++ src/jit/compemu.h
@@ -5,16 +5,19 @@
 #ifndef AMIBERRY_COMPEMU_H
 #define AMIBERRY_COMPEMU_H
 
-#if defined(CPU_arm) || defined(CPU_AARCH64)
+#if defined(CPU_arm) || defined(CPU_AARCH64) || defined(__arm__) || defined(_M_ARM) || \
+	defined(__aarch64__) || defined(_M_ARM64) || defined(_M_ARM64EC)
 #include "arm/compemu_arm.h"
 #elif defined(__x86_64__) || defined(_M_AMD64)
 #include "x86/compemu_x86.h"
 #endif
 
 extern volatile int jit_exception_pending;
-#if defined(CPU_AARCH64) || defined(CPU_x86_64) || defined(__x86_64__) || defined(_M_AMD64)
+#if defined(CPU_AARCH64) || defined(__aarch64__) || defined(_M_ARM64) || defined(_M_ARM64EC) || \
+	defined(CPU_x86_64) || defined(__x86_64__) || defined(_M_AMD64)
 #define JIT_HAS_BUS_ERROR_RECOVERY 1
-#if defined(_WIN32) && defined(CPU_AARCH64) && defined(D)
+#if defined(_WIN32) && (defined(CPU_AARCH64) || defined(__aarch64__) || defined(_M_ARM64) || \
+	defined(_M_ARM64EC)) && defined(D)
 /* mingw-w64 ARM64 setjmp.h declares a member named D in _JUMP_BUFFER.
  * UAE debug headers define D as an empty marker macro, which corrupts that
  * system declaration unless the macro is hidden around the include. */
