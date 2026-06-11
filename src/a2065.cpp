743ec08b5de76cdaa4ebb622379bc96f047ab288
Dimitris Panokostas <midwan@gmail.com>
2026-06-11 20:49:02 +0200

    Fix AMD LANCE chip ID byteswap
    Merged from WinUAE

--- src/a2065.cpp
+++ src/a2065.cpp
@@ -783,10 +783,11 @@ static uae_u16 chip_wget (uaecptr addr)
 				v |= CSR0_ERR;
 			break;
 			// chip id
-			case 88:
+			// vvvvpppp pppppppp ppppmmmm mmmmmmm1
+			case 89:
 			v = 1 << (28 - 16);
 			break;
-			case 89:
+			case 88:
 			v = 0x3003;
 			break;
 		}
