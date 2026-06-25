11c34405df3a2f37fcaeb09ed88a3adc96538693
Dimitris Panokostas <midwan@gmail.com>
2026-06-26 01:13:15 +0200

    libretro: crop overscan borders in the present path

--- src/osdep/libretro/gfx_platform_internal.h
+++ src/osdep/libretro/gfx_platform_internal.h
@@ -45,7 +45,17 @@ static inline bool gfx_platform_present_frame(const SDL_Surface* surface)
 {
 	if (surface) {
 		if (video_cb) {
-			video_cb(surface->pixels, surface->w, surface->h, surface->pitch);
+			const uae_u8* pixels = static_cast<const uae_u8*>(surface->pixels);
+			int w = surface->w;
+			int h = surface->h;
+			libretro_crop crop = libretro_compute_crop();
+			if (crop.active) {
+				pixels += crop.y * surface->pitch
+					+ crop.x * SDL_BYTESPERPIXEL(surface->format);
+				w = crop.w;
+				h = crop.h;
+			}
+			video_cb(pixels, w, h, surface->pitch);
 		} else {
 			static bool warned = false;
 			if (!warned) {
